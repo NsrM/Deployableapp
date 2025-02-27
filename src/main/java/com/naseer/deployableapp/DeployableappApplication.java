4265ea8c9e620fd0f2864b9e49d78efc959f4544
NsrM <naseerthedecent@gmail.com>
2025-02-27 11:17:40 +0530

    Update DeployableappApplication.java

--- src/main/java/com/naseer/deployableapp/DeployableappApplication.java
+++ src/main/java/com/naseer/deployableapp/DeployableappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeployableappApplication {
 
+	// deploy again
+
 	public static void main(String[] args) {
 		SpringApplication.run(DeployableappApplication.class, args);
 	}
